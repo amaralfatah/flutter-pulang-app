475281c77648e0b30e04c05d53dfd14bb9d35a8a
amaralfatah <amaralfatah.main@gmail.com>
2026-06-07 12:32:00 +0700

    test: inisialisasi locale Indonesia pada widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,9 +8,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'package:pulang/app/app.dart';
 
 void main() {
+  setUpAll(() async {
+    await initializeDateFormatting('id_ID', null);
+  });
+
   testWidgets('App should build without errors', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const ProviderScope(child: PulangApp()));
